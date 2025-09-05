981c3866bcd0d8fcba829f1665e136cfa91cb0d2
User <yagmurnov2005@gmail.com>
2025-09-05 15:47:04 +0400

    05.09

---
+++ Pa201lab/Pa201lab/Program.cs
@@ -0,0 +1,13 @@
+﻿int age = 25;
+if (age > 0 && age < 18)
+{
+    Console.WriteLine("Usaq");
+}
+else if (age >= 18 && age < 65)
+{
+    Console.WriteLine("yetiskin");
+}
+else if (age >= 65)
+{
+    Console.WriteLine("yasli");
+}
