5e61e39a9050a87c5c04702bf04390fe1e5f42e8
User <yagmurnov2005@gmail.com>
2026-01-08 12:00:19 +0400

    Added Message Broker template

---
+++ MessageBrokers/WithoutMessageBroker/WithoutMessageBroker.Console/Program.cs
@@ -0,0 +1,16 @@
+﻿// Email gondermek ucun her hansi bir duymeye basin:
+Console.WriteLine("Email göndərmək üçün hər hansı bir düyməyə basın...");
+Console.ReadKey();
+
+Console.WriteLine("Order Melumatları toplanılır...");
+Thread.Sleep(2000);
+
+Console.WriteLine("Email şablon hazırlanır...");
+Thread.Sleep(4000);
+
+Console.WriteLine("Email göndərilir...");
+Thread.Sleep(10000);
+
+Console.WriteLine("Proses tamamlandı.");
+Console.WriteLine("Çıxmaq üçün Enter düyməsinə basın...");
+Console.ReadLine();
