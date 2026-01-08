14fc0d35e13e3c31cc1634fee4ccbfe9bcb8f5cf
User <yagmurnov2005@gmail.com>
2026-01-08 17:32:40 +0400

    added rabbitMq

--- MessageBrokers/WithMessageBroker/WithMessageBroker.Consumer/Program.cs
+++ MessageBrokers/WithMessageBroker/WithMessageBroker.Consumer/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+ConnectionFactory connectionFactory = new()
+{
+    Uri = new Uri("amqps://swfkgzrt:kJcPP4FVQFS88BsWo74YoE1drX8_efmQ@cow.rmq2.cloudamqp.com/swfkgzrt")
+};
+
+using IConnection connection = await connectionFactory.CreateConnectionAsync();
+using IChannel channel = await connection.CreateChannelAsync();
+
+await channel.QueueDeclareAsync(
+    queue: "test",
+    exclusive: false,
+    autoDelete: false
+);
+
+AsyncEventingBasicConsumer consumer = new(channel);
+
+await channel.BasicConsumeAsync(
+            queue: "test",
+            autoAck: true,
+            consumer: consumer);
+
+consumer.ReceivedAsync += async (sender, ev) =>
+{
+    byte[] body = ev.Body.ToArray();
+    string message = System.Text.Encoding.UTF8.GetString(body);
+    Console.WriteLine($"Alinan mesaj: {message}");
+};
+
+Console.Read();
--- MessageBrokers/WithMessageBroker/WithMessageBroker.Producer/Program.cs
+++ MessageBrokers/WithMessageBroker/WithMessageBroker.Producer/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using RabbitMQ.Client;
+
+ConnectionFactory connectionFactory = new()
+{
+    Uri = new Uri("amqps://swfkgzrt:kJcPP4FVQFS88BsWo74YoE1drX8_efmQ@cow.rmq2.cloudamqp.com/swfkgzrt")
+};
+
+using IConnection connection = await connectionFactory.CreateConnectionAsync();
+using IChannel channel = await connection.CreateChannelAsync();
+
+await channel.QueueDeclareAsync(
+    queue: "test",
+    exclusive: false,
+    autoDelete: false
+);
+
+int num = 0;
+while (true)
+{
+    num++;
+    string message = $"Hello  {num}";
+    byte[] body = System.Text.Encoding.UTF8.GetBytes(message);
+    await channel.BasicPublishAsync(
+    exchange: "",
+    routingKey: "test",
+    body: body
+    );
+
+    Thread.Sleep(1000);
+}
